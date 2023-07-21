bb41c5e54ad53036b8dbab555b5650372b3e0340
Steve NOSSE <nossesteve@gmail.com>
2023-07-21 13:45:50 +0200

    update sdk versions

--- lib/screens/pdf_generator_gallery.dart
+++ lib/screens/pdf_generator_gallery.dart
@@ -47,8 +47,7 @@ class _PdfGeneratotGalleryState extends State<PdfGeneratotGallery> {
     Directory tempDir = await getTemporaryDirectory();
     try {
       tempDir.createSync();
-      final file =
-          File("${tempDir.path}/${DateTime.now().millisecondsSinceEpoch}.pdf");
+      final file = File("${tempDir.path}/${DateTime.now().millisecondsSinceEpoch}.pdf");
       await file.writeAsBytes(await pdf.save());
       Navigator.of(context).pop(file);
     } catch (e) {
@@ -56,19 +55,14 @@ class _PdfGeneratotGalleryState extends State<PdfGeneratotGallery> {
       if (e is FileSystemException) {
         message = e.osError?.message ?? 'File System Error';
       }
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(message), backgroundColor: Colors.redAccent));
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message), backgroundColor: Colors.redAccent));
     }
   }
 
   openViewer(File currentItem) {
     int index = files.map((e) => e.path).toList().indexOf(currentItem.path);
 
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (_) =>
-                PhotoViewer(galleryItems: files, selectedItemIndex: index)));
+    Navigator.push(context, MaterialPageRoute(builder: (_) => PhotoViewer(galleryItems: files, selectedItemIndex: index)));
   }
 
   _removeFile(index) {
@@ -82,12 +76,8 @@ class _PdfGeneratotGalleryState extends State<PdfGeneratotGallery> {
   String get itemsTitle {
     late String finalTitle;
     final String countHolder = "{PAGES_COUNT}";
-    var singleTitle = widget.labelsConfig[
-            ScannerLabelsConfig.PDF_GALLERY_FILLED_TITLE_SINGLE] ??
-        '$countHolder Page';
-    var multiTitle = widget.labelsConfig[
-            ScannerLabelsConfig.PDF_GALLERY_FILLED_TITLE_MULTIPLE] ??
-        '$countHolder Pages';
+    var singleTitle = widget.labelsConfig[ScannerLabelsConfig.PDF_GALLERY_FILLED_TITLE_SINGLE] ?? '$countHolder Page';
+    var multiTitle = widget.labelsConfig[ScannerLabelsConfig.PDF_GALLERY_FILLED_TITLE_MULTIPLE] ?? '$countHolder Pages';
     finalTitle = multiTitle;
     if (files.length == 1) {
       finalTitle = singleTitle;
@@ -103,13 +93,7 @@ class _PdfGeneratotGalleryState extends State<PdfGeneratotGallery> {
     var appBar = AppBar(
       automaticallyImplyLeading: false,
       title: Row(
-        children: [
-          if (files.isNotEmpty) Text(itemsTitle),
-          if (files.isEmpty)
-            Text(widget.labelsConfig[
-                    ScannerLabelsConfig.PDF_GALLERY_EMPTY_TITLE] ??
-                "PDF Pages")
-        ],
+        children: [if (files.isNotEmpty) Text(itemsTitle), if (files.isEmpty) Text(widget.labelsConfig[ScannerLabelsConfig.PDF_GALLERY_EMPTY_TITLE] ?? "PDF Pages")],
       ),
     );
     return Scaffold(
@@ -118,68 +102,54 @@ class _PdfGeneratotGalleryState extends State<PdfGeneratotGallery> {
         children: [
           (files.isEmpty)
               ? Center(
-                  child: Text(widget.labelsConfig[
-                          ScannerLabelsConfig.PDF_GALLERY_EMPTY_MESSAGE] ??
-                      'No scanned files available yet!'),
+                  child: Text(widget.labelsConfig[ScannerLabelsConfig.PDF_GALLERY_EMPTY_MESSAGE] ?? 'No scanned files available yet!'),
                 )
               : Container(
                   width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height -
-                      appBar.preferredSize.height,
+                  height: MediaQuery.of(context).size.height - appBar.preferredSize.height,
                   child: CustomScrollView(
                     primary: false,
                     slivers: <Widget>[
                       SliverPadding(
                         padding: const EdgeInsets.all(3.0),
                         sliver: SliverGrid.count(
-                            childAspectRatio: 10.0 / 9.0,
-                            mainAxisSpacing: 1, //horizontal space
-                            crossAxisSpacing: 1, //vertical space
-                            crossAxisCount: 3, //number of images for a row
-                            children: files
-                                .map((image) => Hero(
-                                      tag: image.path,
-                                      child: Stack(
-                                        children: [
-                                          GestureDetector(
-                                            onTap: () => openViewer(image),
-                                            child: Image.file(
-                                              image,
-                                              height: 150,
-                                              width: MediaQuery.of(context)
-                                                      .size
-                                                      .width /
-                                                  3,
-                                              fit: BoxFit.cover,
-                                            ),
-                                          ),
-                                          Positioned(
-                                              right: 5,
-                                              top: 5,
-                                              child: GestureDetector(
-                                                onTap: () => _removeFile(files
-                                                    .map((e) => e.path)
-                                                    .toList()
-                                                    .indexOf(image.path)),
-                                                child: Container(
-                                                  padding: EdgeInsets.all(3),
-                                                  decoration: BoxDecoration(
-                                                      color: Colors.white,
-                                                      border: Border.all(
-                                                          width: 1,
-                                                          color: Colors.grey),
-                                                      borderRadius:
-                                                          BorderRadius.circular(
-                                                              15)),
-                                                  child: Icon(Icons.delete,
-                                                      size: 20,
-                                                      color: Colors.red),
-                                                ),
-                                              ))
-                                        ],
+                          childAspectRatio: 10.0 / 9.0,
+                          mainAxisSpacing: 1, //horizontal space
+                          crossAxisSpacing: 1, //vertical space
+                          crossAxisCount: 3, //number of images for a row
+                          children: files
+                              .map(
+                                (image) => Hero(
+                                  tag: image.path,
+                                  child: Stack(
+                                    children: [
+                                      GestureDetector(
+                                        onTap: () => openViewer(image),
+                                        child: Image.file(
+                                          image,
+                                          height: 150,
+                                          width: MediaQuery.of(context).size.width / 3,
+                                          fit: BoxFit.cover,
+                                        ),
                                       ),
-                                    ))
-                                .toList()),
+                                      Positioned(
+                                        right: 5,
+                                        top: 5,
+                                        child: GestureDetector(
+                                          onTap: () => _removeFile(files.map((e) => e.path).toList().indexOf(image.path)),
+                                          child: Container(
+                                            padding: EdgeInsets.all(3),
+                                            decoration: BoxDecoration(color: Colors.white, border: Border.all(width: 1, color: Colors.grey), borderRadius: BorderRadius.circular(15)),
+                                            child: Icon(Icons.delete, size: 20, color: Colors.red),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        ),
                       ),
                     ],
                   ),
@@ -189,44 +159,30 @@ class _PdfGeneratotGalleryState extends State<PdfGeneratotGallery> {
             child: Container(
               margin: EdgeInsets.symmetric(horizontal: 20),
               width: MediaQuery.of(context).size.width - 40,
-              decoration: BoxDecoration(boxShadow: [
-                BoxShadow(
-                    color: Colors.black.withOpacity(.2),
-                    spreadRadius: 1,
-                    blurRadius: 10)
-              ], borderRadius: BorderRadius.circular(25)),
+              decoration: BoxDecoration(boxShadow: [BoxShadow(color: Colors.black.withOpacity(.2), spreadRadius: 1, blurRadius: 10)], borderRadius: BorderRadius.circular(25)),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   if (files.isNotEmpty)
                     Expanded(
-                        child: _mainControl(context,
-                            color: Colors.blue,
-                            icon: Icons.check,
-                            title: widget.labelsConfig[ScannerLabelsConfig
-                                    .PDF_GALLERY_DONE_LABEL] ??
-                                "Done",
-                            textColor: Colors.white,
-                            onTap: onDone,
-                            radius: BorderRadius.only(
-                                topLeft: Radius.circular(25),
-                                bottomLeft: Radius.circular(25)))),
+                      child: _mainControl(
+                        context,
+                        color: Colors.blue,
+                        icon: Icons.check,
+                        title: widget.labelsConfig[ScannerLabelsConfig.PDF_GALLERY_DONE_LABEL] ?? "Done",
+                        textColor: Colors.white,
+                        onTap: onDone,
+                        radius: BorderRadius.only(topLeft: Radius.circular(25), bottomLeft: Radius.circular(25)),
+                      ),
+                    ),
                   Expanded(
                       child: _mainControl(context,
-                          color:
-                              files.isEmpty ? Colors.blue : Colors.cyanAccent,
+                          color: files.isEmpty ? Colors.blue : Colors.cyanAccent,
                           icon: Icons.add_a_photo,
-                          textColor:
-                              files.isEmpty ? Colors.white : Colors.black,
-                          title: widget.labelsConfig[ScannerLabelsConfig
-                                  .PDF_GALLERY_ADD_IMAGE_LABEL] ??
-                              "Add Image",
+                          textColor: files.isEmpty ? Colors.white : Colors.black,
+                          title: widget.labelsConfig[ScannerLabelsConfig.PDF_GALLERY_ADD_IMAGE_LABEL] ?? "Add Image",
                           onTap: addImage,
-                          radius: files.isEmpty
-                              ? BorderRadius.circular(25)
-                              : BorderRadius.only(
-                                  topRight: Radius.circular(25),
-                                  bottomRight: Radius.circular(25)))),
+                          radius: files.isEmpty ? BorderRadius.circular(25) : BorderRadius.only(topRight: Radius.circular(25), bottomRight: Radius.circular(25)))),
                 ],
               ),
             ),
@@ -236,29 +192,32 @@ class _PdfGeneratotGalleryState extends State<PdfGeneratotGallery> {
     );
   }
 
-  Widget _mainControl(BuildContext context,
-          {required String title,
-          required Function onTap,
-          required Color color,
-          IconData? icon,
-          Color textColor = Colors.black,
-          required BorderRadius radius}) =>
+  Widget _mainControl(
+    BuildContext context, {
+    required String title,
+    required Function onTap,
+    required Color color,
+    IconData? icon,
+    Color textColor = Colors.black,
+    required BorderRadius radius,
+  }) =>
       GestureDetector(
         onTap: () => onTap(),
         child: Container(
-            decoration: BoxDecoration(borderRadius: radius, color: color),
-            padding: EdgeInsets.symmetric(vertical: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                if (icon != null)
-                  Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: Icon(icon, size: 20, color: textColor),
-                  ),
-                Text(title, style: TextStyle(color: textColor))
-              ],
-            ),
-            alignment: Alignment.center),
+          decoration: BoxDecoration(borderRadius: radius, color: color),
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (icon != null)
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Icon(icon, size: 20, color: textColor),
+                ),
+              Text(title, style: TextStyle(color: textColor))
+            ],
+          ),
+          alignment: Alignment.center,
+        ),
       );
 }
